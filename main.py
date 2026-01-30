c921bd0c8195b8e3ea0a02787166af003f59f0e6
Ark121212124 <Arktiko12@gmail.com>
2026-01-30 23:03:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from config import BOT_TOKEN
-import database
 
-from handlers import start, news, subscribe, feedback, admin
+from config import BOT_TOKEN
+import database  # создаёт таблицы при запуске
+
+# handlers
+from handlers import (
+    start,
+    news,
+    subscribe,
+    feedback,
+    admin,
+    contacts
+)
+
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
+    # подключение роутеров
     dp.include_router(start.router)
     dp.include_router(news.router)
+    dp.include_router(contacts.router)
     dp.include_router(subscribe.router)
     dp.include_router(feedback.router)
     dp.include_router(admin.router)
 
-    print("Bot started")
+    print("Bot started...")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
