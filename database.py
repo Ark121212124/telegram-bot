284a91e38a0ae83b1216aa8670d13ed097f55fa7
Ark121212124 <Arktiko12@gmail.com>
2026-01-30 22:13:13 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,22 +1,36 @@
 import sqlite3
 
+# подключение к базе
 conn = sqlite3.connect("bot.db")
 cursor = conn.cursor()
 
+# ===== ТАБЛИЦА НОВОСТЕЙ =====
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS news(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-title TEXT,
-text TEXT,
-photo TEXT,
-link TEXT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    text TEXT,
+    photo TEXT,
+    link TEXT
 )
 """)
 
+# ===== ТАБЛИЦА ПОДПИСЧИКОВ =====
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS subs(
-user_id INTEGER UNIQUE
+    user_id INTEGER UNIQUE
 )
 """)
 
+# ===== ТАБЛИЦА ОБРАЩЕНИЙ =====
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS feedback(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    phone TEXT,
+    message TEXT
+)
+""")
+
+# сохраняем изменения
 conn.commit()
