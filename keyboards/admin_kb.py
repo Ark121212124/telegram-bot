f31191c4f60bcea5b2552d41bb07f61d33ccc6bd
Ark121212124 <Arktiko12@gmail.com>
2026-01-30 21:39:23 +0300

    Update admin_kb.py

--- keyboards/admin_kb.py
+++ keyboards/admin_kb.py
@@ -3,7 +3,8 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 admin_kb = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="➕ Добавить новость")],
-        [KeyboardButton(text="📢 Рассылка")]
+        [KeyboardButton(text="📢 Рассылка")],
+        [KeyboardButton(text="⬅ В меню")]
     ],
     resize_keyboard=True
 )
