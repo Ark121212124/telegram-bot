8e6b29494a8f88efcc6ce2f74610eda6cf42bcde
Ark121212124 <Arktiko12@gmail.com>
2026-01-30 21:25:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
-from handlers import start, news, admin, feedback, subscribe
 import database
+
+from handlers import start, news, subscribe, feedback, admin
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
@@ -10,10 +11,11 @@ async def main():
 
     dp.include_router(start.router)
     dp.include_router(news.router)
-    dp.include_router(admin.router)
-    dp.include_router(feedback.router)
     dp.include_router(subscribe.router)
+    dp.include_router(feedback.router)
+    dp.include_router(admin.router)
 
+    print("Bot started")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
