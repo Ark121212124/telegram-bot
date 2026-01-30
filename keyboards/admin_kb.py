f1e23928944c559d95ec9cfd0cf58ed9ca025597
Ark121212124 <Arktiko12@gmail.com>
2026-01-30 22:42:08 +0300

    Update admin_kb.py

--- keyboards/admin_kb.py
+++ keyboards/admin_kb.py
@@ -1,11 +1,33 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+# главная админ клавиатура
 admin_kb = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="➕ Добавить новость")],
         [KeyboardButton(text="🗂 Управление новостями")],
-        [KeyboardButton(text="📢 Рассылка")],
         [KeyboardButton(text="⬅ В меню")]
+    ],
+    resize_keyboard=True
+)
+
+# клавиатура действий с новостью
+manage_news_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="✏ Редактировать")],
+        [KeyboardButton(text="🗑 Удалить")],
+        [KeyboardButton(text="⬅ В меню")]
+    ],
+    resize_keyboard=True
+)
+
+# клавиатура редактирования полей
+edit_fields_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Заголовок")],
+        [KeyboardButton(text="Текст")],
+        [KeyboardButton(text="Фото")],
+        [KeyboardButton(text="Ссылка")],
+        [KeyboardButton(text="⬅ Назад")]
     ],
     resize_keyboard=True
 )
