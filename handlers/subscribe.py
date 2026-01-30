a95550c10ab89c1c5674b215a3744ee552a5367b
Ark121212124 <Arktiko12@gmail.com>
2026-01-30 21:24:23 +0300

    Update subscribe.py

--- handlers/subscribe.py
+++ handlers/subscribe.py
@@ -1,14 +1,14 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.types import Message
 from database import cursor, conn
 
 router = Router()
 
-@router.message(lambda m: m.text == "🔔 Подписка")
-async def sub(message: Message):
+@router.message(F.text == "🔔 Подписка")
+async def subscribe(message: Message):
     try:
         cursor.execute("INSERT INTO subs VALUES(?)", (message.from_user.id,))
         conn.commit()
-        await message.answer("Вы подписаны!")
+        await message.answer("Вы подписаны на новости!")
     except:
         await message.answer("Вы уже подписаны.")
