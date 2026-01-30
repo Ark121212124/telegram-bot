3a2a9295e1cf0e9b4dbfe0ca50ad88af8d3499eb
Ark121212124 <Arktiko12@gmail.com>
2026-01-30 22:16:05 +0300

    Update states.py

--- utils/states.py
+++ utils/states.py
@@ -1,7 +1,16 @@
 from aiogram.fsm.state import State, StatesGroup
 
+
+# ===== ДОБАВЛЕНИЕ НОВОСТЕЙ =====
 class AddNewsState(StatesGroup):
     title = State()
     text = State()
     photo = State()
     link = State()
+
+
+# ===== ОБРАТНАЯ СВЯЗЬ =====
+class FeedbackState(StatesGroup):
+    name = State()
+    phone = State()
+    message = State()
