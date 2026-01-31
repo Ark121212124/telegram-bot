467c192e47be51ac713b35f12e0fd5fd80d45beb
Ark121212124 <Arktiko12@gmail.com>
2026-01-31 12:31:24 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,4 +1,11 @@
-from aiogram.types import FSInputFile
+from aiogram import Router, F
+from aiogram.types import Message, FSInputFile
+
+from keyboards.user_kb import user_kb
+from config import ADMINS
+
+router = Router()
+
 
 @router.message(F.text == "/start")
 async def start_cmd(message: Message):
@@ -8,10 +15,11 @@ async def start_cmd(message: Message):
 
     await message.answer_photo(
         photo=photo,
-caption=(
-    "Добро пожаловать!\n\n"
-    "Информационный бот посёлка Большая Елховка.\n"
-    "Актуальные новости, контакты учреждений "
-    "и приём обращений граждан."
-)
-
+        caption=(
+            "👋 Добро пожаловать!\n\n"
+            "Информационный бот посёлка Большая Елховка.\n"
+            "Актуальные новости, контакты учреждений "
+            "и приём обращений граждан."
+        ),
+        reply_markup=user_kb(is_admin)
+    )
