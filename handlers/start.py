1671db4ed9b904a5a703559ae171f670d3d4f9e1
Ark121212124 <Arktiko12@gmail.com>
2026-01-31 12:24:59 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,16 +1,13 @@
-from aiogram import Router, F
-from aiogram.types import Message
-from keyboards.user_kb import user_kb
-from config import ADMINS
-
-router = Router()
+from aiogram.types import FSInputFile
 
 @router.message(F.text == "/start")
 async def start_cmd(message: Message):
     is_admin = message.from_user.id in ADMINS
 
+    photo = FSInputFile("images/start.jpg")
+
     await message.answer_photo(
-        photo="https://via.placeholder.com/600x300.png",
-        caption="👋 Добро пожаловать!",
+        photo=photo,
+        caption="👋 Добро пожаловать!\n\nПосёлок Большая Елховка",
         reply_markup=user_kb(is_admin)
     )
