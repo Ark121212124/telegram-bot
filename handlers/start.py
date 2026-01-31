0734e2436376197e8e366edc047620936ba27133
Ark121212124 <Arktiko12@gmail.com>
2026-01-31 12:27:49 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -4,10 +4,14 @@ from aiogram.types import FSInputFile
 async def start_cmd(message: Message):
     is_admin = message.from_user.id in ADMINS
 
-    photo = FSInputFile("images/start.jpg")
+    photo = FSInputFile("images/start.png")
 
     await message.answer_photo(
         photo=photo,
-        caption="👋 Добро пожаловать!\n\nПосёлок Большая Елховка",
-        reply_markup=user_kb(is_admin)
-    )
+caption=(
+    "Добро пожаловать!\n\n"
+    "Информационный бот посёлка Большая Елховка.\n"
+    "Актуальные новости, контакты учреждений "
+    "и приём обращений граждан."
+)
+
