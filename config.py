a73a96562e96682d24e71b6de30e36f250663c0a
Ark121212124 <Arktiko12@gmail.com>
2026-02-01 23:23:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMINS = [6950721969]  # сюда свой Telegram ID
+ADMINS = [5850989673]  # сюда свой Telegram ID
+
 
 
 
